c04290028bbb2184cb69406db9161a29c79247fe
tylerdav <tdavis1433@att.net>
2020-07-01 10:54:56 -0500

    post now shows list of comments

--- Gifter/Repositories/PostRepository.cs
+++ Gifter/Repositories/PostRepository.cs
@@ -17,7 +17,7 @@ namespace Gifter.Repositories
 
         public List<Post> GetAll()
         {
-            return _context.Post.Include(p => p.UserProfile).ToList();
+            return _context.Post.Include(p => p.UserProfile).Include(p => p.Comment).ToList();
         }
 
         public Post GetById(int id)
